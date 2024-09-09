40d7dfa5d6f7d6cb5ba74d1872dd5fb1a15796fc
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-09-09 19:26:52 -0400

    mongodb

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const methodOveride = require('method-override')
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
+    
 })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
