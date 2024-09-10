5059f3253c8de43f2deeac30e81138aab8329338
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-09-09 21:40:49 -0400

    database usernames plz 2

--- passport-config.js
+++ passport-config.js
@@ -29,3 +29,49 @@
 // }
 
 // module.exports = initialize
+
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const collection = require('./config'); // Import your User model
+
+function initialize(passport) {
+    const authenticateUser = async (email, password, done) => {
+        try {
+            // Find user by email using Mongoose
+            const user = await collection.findOne({ email: email });
+            if (!user) {
+                return done(null, false, { message: 'No user with that email' });
+            }
+
+            // Check password
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Password incorrect' });
+            }
+        } catch (error) {
+            return done(error);
+        }
+    };
+
+    // Set up the local strategy for authentication
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+
+    // Serialize user to store user ID in session
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    // Deserialize user from session using user ID
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);
+            done(null, user);
+        } catch (error) {
+            done(error);
+        }
+    });
+}
+
+module.exports = initialize;
