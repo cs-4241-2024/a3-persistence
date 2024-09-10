1f74255dce64b3064aef881079bc9015ce584ff4
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-09-09 20:49:43 -0400

    Revert to "set port"
    This reverts commit c92924a5d8b15ebaec01bc4e8e72cb1e50168bf1.

--- server.js
+++ server.js
@@ -103,8 +103,4 @@ function checkNotAuthenticated(req, res, next) {
     next()
 }
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+app.listen(3000)
