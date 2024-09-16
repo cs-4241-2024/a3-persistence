f4081999592453d5d5684e7a52a3a37718223ba3
smoliner1 <150388911+smoliner1@users.noreply.github.com>
2024-09-16 18:16:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,105 +1,83 @@
-const express = require('express'),
+const express = require("express"),
+      cookie  = require( "cookie-session" ),
+      { MongoClient, ObjectId } = require("mongodb"),
       app = express()
 
-app.use( express.static('views') )
-app.use( express.static('public') )
-app.use( express.json() )
+app.use(express.static('views') )
+app.use(express.static('public') )
+app.use(express.json())
+app.use(express.urlencoded({extended: true }));
 
-const listener = app.listen( process.env.PORT || 3000 )
+// cookie middleware! The keys are used for encryption and should be
+// changed
+app.use( cookie({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
 
-let appdata = [
-  { 'yourname': 'Bob', 'game': 'Galaga', 'score': 10000, rank: 'B'},
-  { 'yourname': 'Chris', 'game': 'Pac-Man', 'score': 20000, rank: 'A'},
-  { 'yourname': 'Sean', 'game': 'Burger Time', 'score': 15000, rank: 'B'} 
-]
+const uri = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.yjbwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const client = new MongoClient( uri )
 
-app.post( '/submit', ( request, response ) => {
-  let dataString = ''
+let collection = null
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+async function run() {
+  await client.connect()
+  collection = await client.db("datatest").collection("test")
 
-  request.on( 'end', function() {
-    const data = JSON.parse( dataString )
 
-  const yourname = data.yourname
-  const game = data.game
-  const score = data.score
-  console.log(data, yourname, game, score);
-  let rank = ''
-  if(Number(score) < 1000) {
-    rank = 'E'
-  } else if(score < 5000) {
-    rank = 'D'
-  } else if(score < 10000) {
-    rank = 'C'
-  } else if(score < 20000) {
-    rank = 'B'
-  } else if(score < 30000) {
-    rank = 'A'
-  } else {
-    rank = 'S'
-  }
+  app.post( '/login', async (req,res)=> {
+  // express.urlencoded will put your key value pairs 
+  // into an object, where the key is the name of each
+  // form field and the value is whatever the user entered
+  // below is *just a simple authentication example* 
+  // for A3, you should check username / password combos in your database
+  let count = await collection.count({username: req.body.username, password: req.body.password}, {limit: 1})
+  if( count ) {
+    // define a variable that we can check in other middleware
+    // the session object is added to our requests by the cookie-session middleware
+    req.session.login = true
+    req.session.username = req.body.username
+    console.log( req.session )
     
-    appdata.push({
-      'yourname': yourname,
-      'game': game,
-      'score': score,
-      'rank': rank
-    })
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, { 'Content-Type': 'application/json'})
-    response.end(JSON.stringify(appdata))
+    // since login was successful, send the user to the main content
+    // use redirect to avoid authentication problems when refreshing
+    // the page or using the back button, for details see:
+    // https://stackoverflow.com/questions/10827242/understanding-the-post-redirect-get-pattern 
+    res.redirect( 'main.html' )
+   }else{
+    // password incorrect, redirect back to login page
+    let values = {username: req.body.username, password: req.body.password};
+    await collection.insertOne(values)
+    req.session.login = true
+    res.redirect( 'main.html' )
+    }
   })
-})
 
-app.post( '/delete', ( request, response ) => {
-  let dataString = ''
-  
-  request.on( 'data', function( data ) {
-      dataString += data 
+  // add some middleware that always sends unauthenicaetd users to the login page
+  app.use( function( req,res,next) {
+    if( req.session.login === true )
+      next()
+    else
+      res.sendFile( __dirname + '/views/index.html' )
   })
-  
-  request.on( 'end', function() {
-  const input = JSON.parse(dataString)
-  const x = appdata.findIndex(item =>
-    item.yourname === input.yourname &&
-    item.game === input.game &&
-    item.score === input.score)
 
-  if (x !== -1){
-    appdata.splice(x, 1)
-  }
-      
-  response.writeHead( 200, { 'Content-Type': 'application/json'}) 
-  response.end(JSON.stringify(appdata))
+  // serve up static files in the directory public
+  app.use( express.static('views') )
+
+  const listener = app.listen( process.env.PORT || 3000 )
+
+  // route to get all docs
+  app.get("/docs", async (req, res) => {
+    if (collection !== null) {
+      const docs = await collection.find({id: req.session.username}).toArray()
+      console.log(docs)
+      res.json( docs )
+    }
   })
-})
 
-app.post( '/modify', ( request, response ) => {
-  let dataString = ''
-  
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
-  
-  request.on( 'end', function() {
-  const input = JSON.parse(dataString)
-  const x = appdata.findIndex(item =>
-    item.yourname === input.yourname1 &&
-    item.game === input.game1 &&
-    item.score === input.score1)
-  console.log(x)
-
-  if (x !== -1){
-    appdata.splice(x, 1)
-    const data = JSON.parse( dataString )
-    const yourname = data.yourname2
-    const game = data.game2
-    const score = data.score2
-    console.log(data, yourname, game, score);
+  app.post( '/submit', async (request,response) => {
+    console.log(request.body)
+    let score = request.body.score
     let rank = ''
     if(Number(score) < 1000) {
       rank = 'E'
@@ -114,16 +92,55 @@ app.post( '/modify', ( request, response ) => {
     } else {
       rank = 'S'
     }
-    
-    appdata.push({
-      'yourname': yourname,
-      'game': game,
-      'score': score,
-      'rank': rank
-    })
-  }
-      
-  response.writeHead( 200, { 'Content-Type': 'application/json'}) 
-  response.end(JSON.stringify(appdata))
+    console.log( request.session )
+    let _id = {id: request.session.username}
+    console.log(_id)
+    let values = {id: request.session.username, yourname: request.body.yourname, game: request.body.game, score: request.body.score, rank: rank};
+    let result = await collection.insertOne(values)
+    //console.log(result)
+    response.json(result)
   })
-})
+  
+  // assumes req.body takes form { _id:5d91fb30f3f81b282d7be0dd } etc.
+  app.post( '/delete', async (request,response) => {
+    console.log(request.body)
+    console.log( request.session )
+    let _id = {id: request.session.username}
+    console.log(_id)
+    let values = {id: request.session.username, yourname: request.body.yourname, game: request.body.game, score: request.body.score}
+    const result = await collection.deleteOne(values)
+  
+    response.json( result )
+  })
+  
+  app.post( '/modify', async (request,response) => {
+    console.log(request.body)
+    let score = request.body.score2
+    let rank = ''
+    if(Number(score) < 1000) {
+      rank = 'E'
+    } else if(score < 5000) {
+      rank = 'D'
+    } else if(score < 10000) {
+      rank = 'C'
+    } else if(score < 20000) {
+      rank = 'B'
+    } else if(score < 30000) {
+      rank = 'A'
+    } else {
+      rank = 'S'
+    }
+    console.log( request.session )
+    let _id = {id: request.session.username}
+    console.log(_id)
+    let values = {id: request.session.username, yourname: request.body.yourname1, game: request.body.game1, score: request.body.score1};
+    const result = await collection.updateOne(values,
+      { $set: {yourname: request.body.yourname2, game: request.body.game2, score: request.body.score2, rank: rank} }
+    )
+
+    response.json( result )
+  })
+  
+}
+
+run().catch(console.dir);
