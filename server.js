b6883e90d068f87023a57e9102968995038cd08e
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-09-15 22:43:44 -0400

    Trying to set up package installs

--- server.js
+++ server.js
@@ -1,3 +1,75 @@
+/**********************
+const express = require('express'),
+  cookie = require('cookie-session'),
+  hbs = require('express-handlebars').engine,
+  app = express()
+
+// use express.urlencoded to get data sent by defaut form actions
+// or GET requests
+app.use(express.urlencoded({ extended: true }))
+//app.use( express.json() )
+
+// we're going to use handlebars, but really all the template
+// engines are equally painful. choose your own poison at:
+// http://expressjs.com/en/guide/using-template-engines.html
+app.engine('handlebars', hbs())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+// cookie middleware! The keys are used for encryption and should be changed
+app.use(cookie({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
+
+app.post('/login', (req, res) => {
+  // express.urlencoded will put your key value pairs 
+  // into an object, where the key is the name of each
+  // form field and the value is whatever the user entered
+  console.log(req.body)
+
+  // below is *just a simple authentication example* 
+  // for A3, you should check username / password combos in your database
+  if (req.body.password === 'test') {
+    // define a variable that we can check in other middleware
+    // the session object is added to our requests by the cookie-session middleware
+    req.session.login = true
+
+    // since login was successful, send the user to the main content
+    // use redirect to avoid authentication problems when refreshing
+    // the page or using the back button, for details see:
+    // https://stackoverflow.com/questions/10827242/understanding-the-post-redirect-get-pattern 
+    res.redirect('main.html')
+  } else {
+    // cancel session login in case it was previously set to true
+    req.session.login = false
+    // password incorrect, send back to login page
+    res.render('index', { msg: 'login failed, please try again', layout: false })
+  }
+})
+
+app.get('/', (req, res) => {
+  res.render('index', { msg: '', layout: false })
+})
+
+// add some middleware that always sends unauthenicaetd users to the login page
+app.use(function (req, res, next) {
+  if (req.session.login === true)
+    next()
+  else
+    res.render('index', { msg: 'login failed, please try again', layout: false })
+})
+
+app.get('/main.html', (req, res) => {
+  res.render('main', { msg: 'success you have logged in', layout: false })
+})
+
+app.listen(process.env.PORT || 3000)
+
+**************/
+
+///////NEW CODE ABOVE FROM PROF GLITCH
+
 const http = require('http'),
   fs = require('fs'),
   // IMPORTANT: you must run `npm install` in the directory for this assignment
