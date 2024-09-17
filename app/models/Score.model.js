4dc4d9ecf23063bc6c941ebc66c60d2170730cba
Sophia Woodward <slwoodward@wpi.edu>
2024-09-16 23:47:10 -0400

    Ported over assignment 2 and started changing architecture

--- app/models/Score.model.js
+++ app/models/Score.model.js
@@ -1,10 +1,15 @@
+const {mongoose} = require('mongoose');
+
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const Score = new Schema({
+const scoreSchema = new Schema({
     objectId: ObjectId,
     name: String,
     points: String,
     rank: Number,
     date: Date
 });
+const Score = mongoose.model('Score', scoreSchema)
+
+module.exports = Score;
