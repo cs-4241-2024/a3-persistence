389458c0e262c7abe8fb58141dfbed5b2802ac16
piperoco23 <proconnell@wpi.edu>
2024-09-18 16:43:26 -0400

    fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 // // FRONT-END (CLIENT) JAVASCRIPT HERE
-
 const submit = async function( event ) {
 
     event.preventDefault()
 
 
+    console.log("I have submitted")
     const name = document.getElementById('name').value
     const cookie = document.getElementById('cookie').value
     const icecream = document.getElementById('icecream').value
@@ -18,14 +18,18 @@ const submit = async function( event ) {
     }else{
         cake = 'no cake!'
     }
-    const input = [name, cookie, icecream, other, cake]
-    const body = JSON.stringify( input )
+    //const input = [name, cookie, icecream, other, cake]
+    //const body = JSON.stringify( input )
 
     const dict = {'name': name, 'cookie': cookie, 'icecream': icecream, 'other': other, 'cake': cake}
+    console.log(dict)
+    const body = JSON.stringify(dict)
+    console.log(body)
 
 
-    const response = await fetch('/handlePost', {
+    const response = await fetch('/submit', {
         method:'POST',
+        headers: { 'Content-Type': 'application/json' },
         body
     })
 
@@ -37,19 +41,24 @@ const submit = async function( event ) {
     const text = JSON.parse(textPre);
     console.log(text)
 
-
-    function addCellTwo(tr, text){
+    //swapping text for dict
+    function addCellTwo(tr, dict){
         const td = tr.insertCell();
-        td.innerHTML = text;
+        td.innerHTML = dict;
         return td;
     }
 
     const row = tableT.insertRow();
-    const nameVar = text[0]
-    const cookieVar = text[1];
-    const icecreamVar = text[2];
-    const otherVar = text[3];
-    const cakeVar = text[4];
+    // const nameVar = text[0]
+    // const cookieVar = text[1];
+    // const icecreamVar = text[2];
+    // const otherVar = text[3];
+    // const cakeVar = text[4];
+    const nameVar = name
+    const cookieVar = cookie
+    const icecreamVar = icecream
+    const otherVar = other
+    const cakeVar = cake
     addCellTwo(row, nameVar)
     addCellTwo(row, cookieVar)
     addCellTwo(row, icecreamVar)
@@ -58,21 +67,6 @@ const submit = async function( event ) {
 
     console.log("row")
 
-
-
-    // const responseData = await fetch('/data',{
-    //     method: 'GET'
-    // })
-
-    //const textPre = await responseData.text();
-    //const text = JSON.parse(textPre);
-
-    //console.log(text)
-    //console.log(text[0].name.value)
-    //fillTable(text)
-    //fillTable(responseData)
-
-    //console.log('text:',text)
 }
 
 const fillTable = function( dict ) {
@@ -88,7 +82,8 @@ const fillTable = function( dict ) {
         return td;
     }
 
-
+    console.log("fill table")
+    console.log(dict)
     dict.forEach(function (item){
         const row = table.insertRow();
         const name = item["name"];
@@ -104,18 +99,132 @@ const fillTable = function( dict ) {
     })
 }
 
+const deleteVar = async function(event){
+    console.log("I am deleteing")
+    event.preventDefault()
 
+    console.log("delete")
+
+    const name = document.getElementById('name').value
+    const cookie = document.getElementById('cookie').value
+
+    const dict = {'name': name, 'cookie': cookie}
+    const body = JSON.stringify(dict)
+
+
+    const response = await fetch('/remove', {
+        method:'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body
+    })
+    console.log("im here 1")
+    const tableT = document.getElementById('target')
+    console.log("im here 2")
+    deleteRowItem()
+    function deleteRowItem(){
+        var i;
+        for(i = 0; i < tableT.rows.length; i++){
+            console.log("inside for")
+            var row = tableT.rows[i]
+            console.log(row)
+            var cellOne = row.cells[0]
+            var cellTwo = row.cells[1]
+            var cellOneValue = cellOne.innerHTML
+            var cellTwoValue = cellTwo.innerHTML
+            console.log(cellOneValue)
+            console.log(cellTwoValue)
+            if(cellOneValue == name && cellTwoValue == cookie){
+                tableT.deleteRow(i)
+            }
+        }
+    }
+}
+
+const updateInfo = async function (event){
+    event.preventDefault()
+    const name = document.getElementById('name').value
+    const cookie = document.getElementById('cookie').value
+    const icecream = document.getElementById('icecream').value
+    const other = document.getElementById('other').value
+    const dict = {'name': name, 'cookie': cookie, 'icecream': icecream, 'other': other}
+    const body = JSON.stringify(dict)
+
+    const response = await fetch('/update', {
+        method:'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body
+    })
+
+    const tableT = document.getElementById('target')
+
+    updateAppRow()
+
+    function updateAppRow(){
+        console.log("Im in the for")
+        var i;
+        for(i = 0; i < tableT.rows.length; i++){
+            var row = tableT.rows[i]
+            console.log(row)
+            var cellOne = row.cells[0]
+            var cellTwo = row.cells[1]
+            var cellThree = row.cells[2]
+            var cellFour = row.cells[3]
+            var cellOneValue = cellOne.innerHTML
+            var cellTwoValue = cellTwo.innerHTML
+            var cellThreeValue = cellThree.innerHTML
+            var cellFourValue = cellFour.innerHTML
+            console.log(cellOneValue)
+            console.log(cellTwoValue)
+            if(cellOneValue == name){
+                console.log("changing" +cellTwo.innerHTML)
+                cellTwo.innerHTML = cookie;
+                console.log("changing" +cellTwo.innerHTML)
+                cellThree.innerHTML = icecream;
+                console.log("changing" +cellThree.innerHTML)
+                cellFour.innerHTML = other;
+                console.log("changing" +cellFour.innerHTML)
+
+            }
+        }
+    }
+
+    //console.log("hit the updatebutton")
+
+}
+
+
+//
 window.onload = function() {
     //This allows you to bind you button to the event handler function submit above
-    const buttonVariable = document.getElementById("button");
-    if(buttonVariable.addEventListener)
-        buttonVariable.addEventListener("click",submit,false);
-    else if(buttonVariable.attachEvent)
-        buttonVariable.attachEvent('onclick',submit)
+    // const buttonVariable = document.getElementById("button");
+    // if(buttonVariable.addEventListener)
+    //     buttonVariable.addEventListener("click",submit,false);
+    // else if(buttonVariable.attachEvent)
+    //     buttonVariable.attachEvent('onclick',submit)
+    const buttonVariable = document.getElementById("button")
+    buttonVariable.onclick = submit
 
+    const buttonDelete = document.getElementById("deletebutton")
+    buttonDelete.onclick = deleteVar
+
+    const buttonUpdate = document.getElementById("updateButton")
+    buttonUpdate.onclick = updateInfo
+
+    // const buttonDelete = document.getElementbyId("deletebutton")
+    // if(buttonDelete.addEventListener){
+    //   buttonDelete.addEventListener("click1",deleteVar,false);
+    // }
+    // else if(buttonDelete.attachEvent){
+    //   buttonDelete.attachEvent('onclick1',deleteVar)
+    // }
+
+    console.log("response")
     fetch('/data',{
         method: 'GET'
     }).then(response => response.json()).then(json=>{fillTable(json)})
+    //then(response => console.log("response"))
+    const uName = sessionStorage.getItem('uname')
+    console.log(uName)
 
     //button.onclick = submit
     //submit()
--- server.js
+++ server.js
@@ -1,137 +1,114 @@
 const express = require('express'),
-    app = express(),
-    cookiedata = []
+    { MongoClient, ObjectId } = require('mongodb'),
+    cookie = require('cookie-session'),
+    app = express()
 
-app.use(express.static('views'))
-app.use(express.static('public'))
-app.use(express.json())
+// const express = require('express'),
+//       app = express(),
+//       cookiedata = []
 
-app.post('/submit', (req, res) => {
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    cookiedata.push(req.body)
-    res.end(JSON.stringify(cookiedata))
+// app.use(express.static('./public'))
+// app.use(express.json())
+
+app.use(express.urlencoded({ extended:true }))
+app.use( express.static(__dirname + '/public') )
+app.use( express.json() )
+
+
+const uri = "mongodb+srv://admin:adminPassword1@cluster0.5idai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient (uri)
+
+app.use( cookie({
+    name: 'session',
+    keys: ['keycookie1', 'keycookie2']
+}))
+
+let collectionTwo = client.db("CookieDatabase").collection("UserPass")
+//console.log(collectionTwo)
+
+app.post( '/login', async (req,res)=> {
+    //console.log( req.body )
+
+    if(collectionTwo!== null){
+        const passUser = await collectionTwo.find({username:req.body.username}).toArray()
+        //console.log(passUser[0])
+        if(passUser !== null){
+            // console.log(passUser[0].password)
+            // console.log(passUser.password)
+            // console.log(req.body.password)
+            if(passUser[0].password === req.body.password){
+                req.session.login = true
+                res.redirect( 'main.html' )
+            }
+        }else{
+            req.session.login = false
+            res.sendFile( __dirname + '/public/main.html' )
+        }
+    }
 })
 
-app.list(process.env.PORT)
 
-const listener = app.listen (process.env.PORT || 3000)
+app.use( function( req,res,next) {
+    if( req.session.login === true )
+        next()
+    else
+        res.sendFile( __dirname + '/public/main.html' )
+})
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:<db_password>@cluster0.5idai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+let collection = null
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
+    await client.connect()
+    collection = await client.db("CookieDatabase").collection("Cookies")
+    //console.log(collection)
+
 }
-run().catch(console.dir);
+run()
+
+app.post( '/submit', async( req, res ) => {
+    const result = await collection.insertOne( req.body )
+    //console.log(req.body)
+    res.json( result )
+    //res.writeHead( 200, { 'Content-Type': 'application/json'})
+    //cookiedata.push( req.body )
+    //res.end( JSON.stringify( cookiedata ) )
+})
+
+
+app.get('/data', async (req,res) => {
+    //res.sendFile(__dirname + 'public', '/main.html')
+    //console.log("collection is null")
+    if (collection !== null) {
+        //const passUserTest = await collectionTwo.find({username:req.body.username}).toArray()
+        const docs = await collection.find({}).toArray()
+        //console.log(docs[1])
+        //name:req.body.name
+        //console.log(docs)
+        //console.log("docs:" + docs)
+        res.json( docs )
+    }
+})
+
+// assumes req.body takes form { _id:5d91fb30f3f81b282d7be0dd } etc.
+app.delete( '/remove', async (req,res) => {
+    const result = await collection.deleteOne(req.body)
+    res.json( result )
+})
+
+app.put( '/update', async (req,res) => {
+    const result = await collection.updateOne(
+        //{ _id: new ObjectId( req.body._id ) },
+        {name:req.body.name,},
+        { $set:{ cookie:req.body.cookie,
+                icecream:req.body.icecream,
+                other:req.body.other} }
+    )
+    res.json( result )
+})
 
 
 
+const listener = app.listen( process.env.PORT || 3000 )
 
-
-
-// const http = require( 'http' ),
-//     fs   = require( 'fs' ),
-//     // IMPORTANT: you must run `npm install` in the directory for this assignment
-//     // to install the mime library if you're testing this on your local machine.
-//     // However, Glitch will install it automatically by looking in your package.json
-//     // file.
-//     mime = require( 'mime' ),
-//     dir  = 'public/',
-//     port = 3000
-//
-// let appdata =  [
-//     { 'name': 'Piper', 'cookie':'chocolate chip', 'icecream': 'vanilla', 'other':'', 'cake':'vanilla cake'},
-//     { 'name': 'James', 'cookie':'oatmeal no raisin', 'icecream': 'chocolate', 'other':'', 'cake':'chocolate cake'},
-//     { 'name': 'Sky', 'cookie':'sugar', 'icecream': 'vanilla', 'other':'', 'cake':'vanilla cake'}
-// ]
-//
-// const server = http.createServer( function (request, response ) {
-//     if( request.method === 'GET' ) {
-//         handleGet( request, response )
-//     }else if( request.method === 'POST' ){
-//         handlePost( request, response )
-//     }
-// })
-//
-//
-// const handleGet = function( request, response ) {
-//     const filename = dir + request.url.slice( 1 )
-//     if( request.url === '/' ) {
-//         sendFile( response, 'public/index.html' )
-//     }else if (request.url === '/data'){
-//         response.end(JSON.stringify(appdata))
-//     }
-//     else{
-//         sendFile( response, filename )
-//     }
-// }
-//
-// const handlePost = function( request, response ) {
-//     let dataString = ''
-//
-//     //console.log(appdata)
-//     request.on( 'data', function( data ) {
-//         console.log(data)
-//         dataString += data
-//     })
-//     request.on( 'end', function() {
-//         //console.log( JSON.parse( dataString ) )
-//         const data = JSON.parse(dataString)
-//         const tempDict = {'name':data[0], 'cookie':data[1], 'icecream':data[2], 'other':data[3], 'cake':data[4]}
-//         appdata = appdata.concat(tempDict)
-//         //appdata.push(tempDict)
-//
-//         //const data = JSON.parse( dataString )
-//         // ... do something with the data here!!!
-//         //appdata.push(response)
-//         //appdata.push(data)
-//         //appdata = appdata + response
-//
-//         response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-//         response.end(JSON.stringify(data))
-//     })
-// }
-//
-// const sendFile = function( response, filename ) {
-//     const type = mime.getType( filename )
-//
-//     fs.readFile( filename, function( err, content ) {
-//
-//         // if the error = null, then we've loaded the file successfully
-//         if( err === null ) {
-//
-//             // status code: https://httpstatuses.com
-//             response.writeHeader( 200, { 'Content-Type': type })
-//             response.end( content )
-//
-//         }else{
-//
-//             // file not found, error code 404
-//             response.writeHeader( 404 )
-//             response.end( '404 Error: File Not Found' )
-//
-//         }
-//     })
-// }
-//
-// server.listen( process.env.PORT || port )
