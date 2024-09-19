28a924c3ff20b2cf8ce562e3546206661296913f
jeremymkurtz <kurtzjeremy126@gmail.com>
2024-09-19 05:24:36 -0400

    setup for altering data

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.delete( '/remove', async (req,res) => {
 app.post( '/update', async (req,res) => {
   const result = await collection.updateOne(
       { _id: new ObjectId( req.body._id ) },
-      { $set:{ name:req.body.name } }
+      { $set: req.body }
   )
 
   res.json( result )
@@ -127,70 +127,17 @@ app.post( '/update', async (req,res) => {
 
 
 
-
-const appdata = [
-  { name: 'Jeremy', clickCount: 10, points: 1000 },
-]
-
-app.get('/getData', (req, res) => {
-  res.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-  res.end(JSON.stringify(appdata))
-})
-
-app.post('/submit', async (req, res) => {
-  const json = req.body
-  console.log(json)
-  let score = json.clickCount*100
-
-
-
-  // add a 'json' field to our request object
-  // this field will be available in any additional
-  // routes or middleware.
-  appdata.push( {name: json.name, clickCount: json.clickCount, points: score }  )
-
-  const result = await collection.insertOne( {name: json.name, clickCount: json.clickCount, points: score } )
-  res.json( result )
-
-  // res.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-  // res.end(JSON.stringify(appdata))
-})
-
-
-
-
-app.delete('/deleteRow', async (req, res) => {
-  const json = req.body
-  console.log(json)
-  let index = Number(json.index)
-
-  appdata.splice(index, 1)
-
-  const result = await collection.deleteOne({
-    _id:new ObjectId( req.body._id )
-  })
-
-  res.json( result )
-
-  // add a 'json' field to our request object
-  // this field will be available in any additional
-  // routes or middleware.
-
-  res.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-  res.end(JSON.stringify(appdata))
-})
-
 app.put('/alterRow', async (req, res) => {
 
   const json = req.body
   console.log(json)
   let index = Number(json.index)
 
-  const targetObject = appdata.find( (row, i) => i === index )
+  // const targetObject = appdata.find( (row, i) => i === index )
 
-  targetObject.name = json.name
-  targetObject.clickCount = json.clickCount
-  targetObject.points = json.clickCount*100
+  // targetObject.name = json.name
+  // targetObject.clickCount = json.clickCount
+  // targetObject.points = json.clickCount*100
 
   const result = await collection.updateOne(
       { _id: new ObjectId( req.body._id ) },
@@ -204,7 +151,7 @@ app.put('/alterRow', async (req, res) => {
   // routes or middleware.
 
   res.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-  res.end(JSON.stringify(appdata))
+  // res.end(JSON.stringify(appdata))
 })
 
 app.listen( process.env.PORT || 3000 )
