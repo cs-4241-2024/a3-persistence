38a35568034e7b0279ee1ead0ae52063c798f8b2
chooboy <chooboy05@gmail.com>
2024-09-19 01:57:29 -0400

    loadtable and submit mongo
    ./loadTable and ./submit now work with the mongoDB database. Also updated resetfnc to work with the database.

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ const logger = (req,res,next) => {
   })
 }*/
 
+let profile
+let tablerows = ["", "", "", "", "", "", "", "", "", "", ""]
+
 const lvl1 = [
   { 'level': "Wizard Level:", 'available': 1, 'used': "", 'remaining': ""},
   { 'level': "Level", 'available': "Total", 'used': "Used", 'remaining': "Remaining"},
@@ -313,73 +316,81 @@ const lvl20 = [
   { 'level': 9, 'available': 1, 'used': 0, 'remaining': 0}
 ]
 
-const resetfnc = function(level) {
-
+const resetfnc = async function(level) {
+let copied
   switch (level) {
     case 1:
-      appdata = JSON.parse(JSON.stringify(lvl1))
+      copied = lvl1
       break
     case 2:
-      appdata = JSON.parse(JSON.stringify(lvl2))
+      copied = lvl2
       break
     case 3:
-      appdata = JSON.parse(JSON.stringify(lvl3))
+      copied = lvl3
       break
     case 4:
-      appdata = JSON.parse(JSON.stringify(lvl4))
+      copied = lvl4
       break
     case 5:
-      appdata = JSON.parse(JSON.stringify(lvl5))
+      copied = lvl5
       break
     case 6:
-      appdata = JSON.parse(JSON.stringify(lvl6))
+      copied = lvl6
       break
     case 7:
-      appdata = JSON.parse(JSON.stringify(lvl7))
+      copied = lvl7
       break
     case 8:
-      appdata = JSON.parse(JSON.stringify(lvl8))
+      copied = lvl8
       break
     case 9:
-      appdata = JSON.parse(JSON.stringify(lvl9))
+      copied = lvl9
       break
     case 10:
-      appdata = JSON.parse(JSON.stringify(lvl10))
+      copied = lvl10
       break
     case 11:
-      appdata = JSON.parse(JSON.stringify(lvl11))
+      copied = lvl11
       break
     case 12:
-      appdata = JSON.parse(JSON.stringify(lvl12))
+      copied = lvl12
       break
     case 13:
-      appdata = JSON.parse(JSON.stringify(lvl13))
+      copied = lvl13
       break
     case 14:
-      appdata = JSON.parse(JSON.stringify(lvl14))
+      copied = lvl14
       break
     case 15:
-      appdata = JSON.parse(JSON.stringify(lvl15))
+      copied = lvl15
       break
     case 16:
-      appdata = JSON.parse(JSON.stringify(lvl16))
+      copied = lvl16
       break
     case 17:
-      appdata = JSON.parse(JSON.stringify(lvl17))
+      copied = lvl17
       break
     case 18:
-      appdata = JSON.parse(JSON.stringify(lvl18))
+      copied = lvl18
       break
     case 19:
-      appdata = JSON.parse(JSON.stringify(lvl19))
+      copied = lvl19
       break
     case 20:
-      appdata = JSON.parse(JSON.stringify(lvl20))
+      copied = lvl20
       break
     default:
       console.log("Something went wrong with the reset.")
   }
   
+  for (let i = 0; i < 11; i++) {
+    await userdata.updateOne( {_id : tablerows[i]}, 
+                                        {$set: {'level':copied[i].level,
+                                        'available':copied[i].available,
+                                        'used':copied[i].used,
+                                        'remaining':copied[i].remaining}})
+  }
+
 }
 
 let appdata = [
@@ -448,6 +459,20 @@ app.post( '/login', async (req, res ) => {
     if (rightlogin) {
       //log in
       console.log(req.body, " logging in!")
+      profile = rightlogin
+
+      tablerows[0] = profile.zero
+      tablerows[1] = profile.one
+      tablerows[2] = profile.two
+      tablerows[3] = profile.three
+      tablerows[4] = profile.four
+      tablerows[5] = profile.five
+      tablerows[6] = profile.six
+      tablerows[7] = profile.seven
+      tablerows[8] = profile.eight
+      tablerows[9] = profile.nine
+      tablerows[10] = profile.ten
+
       res.sendFile(path.join(__dirname, 'public', 'wizard.html'))
     } else {
       //tell them they got password wrong
@@ -455,25 +480,36 @@ app.post( '/login', async (req, res ) => {
     }
   } else {
     //TODO creates account, need to add notification
-    let newlogin = (req.body)
+    profile = (req.body)
     //adds the ids of each data row to the login so they can be found
-    newlogin.zero = (await userdata.insertOne(appdata[0])).insertedId
-    newlogin.one = (await userdata.insertOne(appdata[1])).insertedId
-    newlogin.two = (await userdata.insertOne(appdata[2])).insertedId
-    newlogin.three = (await userdata.insertOne(appdata[3])).insertedId
-    newlogin.four = (await userdata.insertOne(appdata[4])).insertedId
-    newlogin.five = (await userdata.insertOne(appdata[5])).insertedId
-    newlogin.six = (await userdata.insertOne(appdata[6])).insertedId
-    newlogin.seven = (await userdata.insertOne(appdata[7])).insertedId
-    newlogin.eight = (await userdata.insertOne(appdata[8])).insertedId
-    newlogin.nine = (await userdata.insertOne(appdata[9])).insertedId
-    newlogin.ten = (await userdata.insertOne(appdata[10])).insertedId
-    
-    logins.insertOne(newlogin)
+    profile.zero = (await userdata.insertOne(appdata[0])).insertedId
+    profile.one = (await userdata.insertOne(appdata[1])).insertedId
+    profile.two = (await userdata.insertOne(appdata[2])).insertedId
+    profile.three = (await userdata.insertOne(appdata[3])).insertedId
+    profile.four = (await userdata.insertOne(appdata[4])).insertedId
+    profile.five = (await userdata.insertOne(appdata[5])).insertedId
+    profile.six = (await userdata.insertOne(appdata[6])).insertedId
+    profile.seven = (await userdata.insertOne(appdata[7])).insertedId
+    profile.eight = (await userdata.insertOne(appdata[8])).insertedId
+    profile.nine = (await userdata.insertOne(appdata[9])).insertedId
+    profile.ten = (await userdata.insertOne(appdata[10])).insertedId
+
+    tablerows[0] = profile.zero
+    tablerows[1] = profile.one
+    tablerows[2] = profile.two
+    tablerows[3] = profile.three
+    tablerows[4] = profile.four
+    tablerows[5] = profile.five
+    tablerows[6] = profile.six
+    tablerows[7] = profile.seven
+    tablerows[8] = profile.eight
+    tablerows[9] = profile.nine
+    tablerows[10] = profile.ten
+
+    await logins.insertOne(profile)
     res.sendFile(path.join(__dirname, 'public', 'wizard.html'))
   }
 
-  
 })
 
 app.post( '/submit', (req, res ) => {
@@ -484,16 +520,27 @@ app.post( '/submit', (req, res ) => {
   res.end(JSON.stringify({lvl: req.body.payload}))
 })
 
-app.post( '/loadTable', (req, res ) => {
+app.post( '/loadTable', async (req, res ) => {
   if (req.body.payload > 1) {
-    let datarow = appdata[req.body.payload]
-    datarow.remaining = datarow.available - datarow.used
+    let datarow = await userdata.findOne({"_id": tablerows[req.body.payload]})
+    let avail = datarow.available
+    let use = datarow.used
+    userdata.updateOne({"_id": tablerows[req.body.payload]}, {$set: {remaining: (avail-use)}})
   }
+  let datarow = await userdata.findOne({"_id": tablerows[req.body.payload]})
+  console.log(datarow)
+  let avail = datarow.available
+  let use = datarow.used
+  let lvl = datarow.level
+  let remainin = datarow.remaining
+
+  console.log(tablerows[req.body.payload])
+  console.log(datarow)
   res.writeHead( 200, "OK", {'Content-Type': 'text' })
-  let sending = [appdata[req.body.payload].level, 
-                appdata[req.body.payload].available, 
-                appdata[req.body.payload].used, 
-                appdata[req.body.payload].remaining]
+  let sending = [lvl, 
+                avail, 
+                use, 
+                remainin]
   res.end(JSON.stringify(sending))
 })
 
