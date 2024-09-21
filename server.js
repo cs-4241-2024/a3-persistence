b46cd3e2a98a21503d69c331d9d3b193703787c4
krutgoat <krutics@outlook.com>
2024-09-21 19:29:03 -0400

    express functionality, gitignore

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const express = require ("express");
 const app = express();
-const port = 3000;
+const { MongoClient, ObjectId } = require("mongodb"); // objID is for db key 
+//const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"))
 app.use(express.json());
-
 app.use(express.urlencoded({extended: true})); // gets data sent by defaut form actions
 
-// app.use applies whatever function in the parenthesis to every request
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.gpytm.mongodb.net/`
+console.log(uri);
+const client = new MongoClient(uri) // Create a MongoClient
 
-app.listen( process.env.PORT || port );
+let collection = null
+
+async function run() {
+  await client.connect()
+  collection = await client.db("a3-database").collection("a3")
+}
+run()
+
+// route to get all docs
+app.get("/docs", async (req, res) => {
+  if (collection !== null) { // if you already connected
+    const docs = await collection.find({}).toArray() // .find is a query; empty {} means to return everything in an array
+    res.json(docs) // convert array into JSON for response
+  }
+})
+
+app.listen(3000)
+
+
+
+// ------------------------------------------------------------------------------------------
+
+
+
+
+// app.use applies whatever function in the parenthesis to every request
 
 let appdata = [] // name price quantity total
 //{"name": "apple", "price": "1.00", "quantity": "5", "total": "5.00", "id":"0"}
@@ -26,32 +53,32 @@ function validateForm(x) { // severside function for handling invalid (blank) da
   }
 }
 
-// get login page
+// // get login page
+// // app.get("/", (req, res) => {
+// //   res.sendFile("login.html", { root: "public" });
+// // });
+
+
+// // get main HTML page
 // app.get("/", (req, res) => {
-//   res.sendFile("login.html", { root: "public" });
+//   res.sendFile("index.html")
 // });
 
 
-// get main HTML page
-app.get("/", (req, res) => {
-  res.sendFile("index.html")
-});
-
-
-// GET request for results
-app.get('/results', (req, res) => {
-  if (validateForm) {
-    res.json(appdata);
-    console.log("Results updated.");
-  }
-  else {
-    console.log("Results failed to update. Please check your inputs.");
-  }
-});
+// // GET request for results
+// app.get('/results', (req, res) => {
+//   if (validateForm) {
+//     res.json(appdata);
+//     console.log("Results updated.");
+//   }
+//   else {
+//     console.log("Results failed to update. Please check your inputs.");
+//   }
+// });
 
 
 let nextId = 1;
-app.post('/', (req, res) => { // POST request to add new item
+app.post('/add', (req, res) => { // POST request to add new item
   if (validateForm) {
     const newEntry = { ...req.body, id: nextId++ };
     newEntry.total = newEntry.price * newEntry.quantity;
@@ -63,28 +90,27 @@ app.post('/', (req, res) => { // POST request to add new item
     console.log("Failed to add item. Please check your inputs.");
   }
 
-  
 });
 
 
-app.delete('/remove/:id', (req, res) => { // DELETE request
-  const id = parseInt(req.params.id, 10);
-  appdata = appdata.filter(entry => entry.id !== id);
-  res.json(appdata);
-  console.log(`Deleting entry with ID: ${id}`);
-});
+// app.delete('/remove/:id', (req, res) => { // DELETE request
+//   const id = parseInt(req.params.id, 10);
+//   appdata = appdata.filter(entry => entry.id !== id);
+//   res.json(appdata);
+//   console.log(`Deleting entry with ID: ${id}`);
+// });
 
-app.put("/update/:id", async (req, res) => { // PUT request (for editing)
-  const id = parseInt(req.params.id, 10);
-  const entryIndex = appdata.findIndex(entry => entry.id === id);
+// app.put("/update/:id", async (req, res) => { // PUT request (for editing)
+//   const id = parseInt(req.params.id, 10);
+//   const entryIndex = appdata.findIndex(entry => entry.id === id);
 
-  if (entryIndex === -1) { // checks if findIndex failed
-    return res.status(404).send('Entry not found');
-  }
+//   if (entryIndex === -1) { // checks if findIndex failed
+//     return res.status(404).send('Entry not found');
+//   }
   
-  const updatedEntry = { ...req.body, id }; // spreads the array into individual elements
-  updatedEntry.total = updatedEntry.price * updatedEntry.quantity;
-  appdata[entryIndex] = updatedEntry;
+//   const updatedEntry = { ...req.body, id }; // spreads the array into individual elements
+//   updatedEntry.total = updatedEntry.price * updatedEntry.quantity;
+//   appdata[entryIndex] = updatedEntry;
   
-  res.json(appdata); // sending the PUT response back to server
-});
+//   res.json(appdata); // sending the PUT response back to server
+// });
